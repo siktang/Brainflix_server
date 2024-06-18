fc4fa1105eb29603a34f5704f8086b7f8fb18cb0
Denise Deng <siktang1221@gmail.com>
2024-06-18 09:45:42 -0400

    Updated get videoList in video router

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import express from "express";
-import fs from "fs";
 import cors from "cors";
 import videoRouter from "./routes/videos.js";
 import "dotenv/config";
@@ -8,12 +7,12 @@ let { PORT } = process.env;
 PORT = PORT || 8080;
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
-app.use('/images',express.static("public"));
+app.use('/images',express.static("./public/images"));
 app.use("/videos", videoRouter); 
-
 
 app.listen(PORT, () => {
     console.log(`App is running on port ${PORT}`);
--- routes/videos.js
+++ routes/videos.js
@@ -1,28 +1,35 @@
 import express from "express";
-import fs from "fs"; 
+import fs, { read } from "fs"; 
 
 const router = express.Router();
 
-
-
 // functions for reading and writing data
 const readVideos = () => {
-    const videosFile = fs.readFileSync("../data/videos.json");
+    const videosFile = fs.readFileSync("./data/videos.json");
     const videosData = JSON.parse(videosFile);
+    console.log(videosData);
     return videosData;
 }
 
 const writeVideos = (data) => {
     const convertedData = JSON.stringify(data);
-    fs.writeFileSync("../data/videos.json", convertedData);
+    fs.writeFileSync("./data/videos.json", convertedData);
 }
-
 
 // routes for getting and posting
 
-router.get("/", (req, res) => {
+router.get("/", (_req, res) => {
     const videoListData = readVideos();
-    res.json(videoListData);
+
+    const filteredData = videoListData.map((video) => {
+        return {
+            id: video.id,
+            title: video.title,
+            channel: video.channel,
+            image: video.image,
+        };
+    })
+    res.json(filteredData);
 })
 
 
